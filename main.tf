590de44bb8a259fdc381f78cd7a3ae3e9ce77b79
romdhane <rglaa@bigapps.fr>
2023-01-16 18:25:49 +0100

    create 2 seperates route tables and another nat

--- main.tf
+++ main.tf
@@ -52,28 +52,39 @@ resource "aws_route_table" "public_route_table" {
   }
 }
 
-resource "aws_route_table" "private_route_table" {
+resource "aws_route_table" "private_route_table1" {
   vpc_id = aws_vpc.main.id
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.example.id
+    nat_gateway_id = aws_nat_gateway.nat1.id
   }
 }
-
-
+resource "aws_route_table" "private_route_table2" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat2.id
+  }
+}
 
 
 resource "aws_eip" "elastic_ip_adress_nat_gw" {
   vpc = true
 }
-resource "aws_eip" "nat2" {
+resource "aws_eip" "elastip_ip_adress_nat_gw_2" {
   vpc      = true
 }
 
-resource "aws_nat_gateway" "example" {
+resource "aws_nat_gateway" "nat1" {
   allocation_id = aws_eip.elastic_ip_adress_nat_gw.id
   subnet_id     = aws_subnet.public_subnet1.id
 }
+
+resource "aws_nat_gateway" "nat2" {
+  allocation_id = aws_eip.elastip_ip_adress_nat_gw_2.id
+  subnet_id     = aws_subnet.public_subnet2.id
+}
+
 
 
 resource "aws_route_table_association" "public_association1" {
