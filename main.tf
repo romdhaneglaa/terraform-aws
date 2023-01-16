3c5a53f57f4a42087f1802d5b528d085235b42a8
romdhane <rglaa@bigapps.fr>
2023-01-16 17:48:20 +0100

    create vpc and 4 subsnets

--- main.tf
+++ main.tf
@@ -1,23 +1,100 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 
-  required_version = ">= 1.2.0"
 }
-
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-0b5eea76982371e91"
-  instance_type = "t2.micro"
+resource "aws_subnet" "public_subnet1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    name = "public_subnet1"
+  }
+
+}
+resource "aws_subnet" "public_subnet2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    name = "public_subnet2"
   }
 }
+
+resource "aws_subnet" "private_subnet1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    name = "private_subnet1"
+  }
+}
+resource "aws_subnet" "private_subnet2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    name = "private_subnet2"
+  }
+}
+
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.example.id
+  }
+}
+
+
+
+
+resource "aws_eip" "elastic_ip_adress_nat_gw" {
+  vpc = true
+}
+resource "aws_eip" "nat2" {
+  vpc      = true
+}
+
+resource "aws_nat_gateway" "example" {
+  allocation_id = aws_eip.elastic_ip_adress_nat_gw.id
+  subnet_id     = aws_subnet.public_subnet1.id
+}
+
+
+resource "aws_route_table_association" "public_association1" {
+  subnet_id      = aws_subnet.public_subnet1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+resource "aws_route_table_association" "public_association2" {
+  subnet_id      = aws_subnet.public_subnet2.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+
+resource "aws_route_table_association" "private_association1" {
+  subnet_id      = aws_subnet.private_subnet1.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+resource "aws_route_table_association" "private_association2" {
+  subnet_id      = aws_subnet.private_subnet2.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+
+
+
