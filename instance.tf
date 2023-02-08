88c3afa3a346876da3a9fe56603dbdfe23fc4f9c
romdhane <rglaa@bigapps.fr>
2023-02-08 23:37:30 +0100

    add gitignore file

--- instance.tf
+++ instance.tf
@@ -31,44 +31,44 @@ resource "aws_instance" "public" {
   key_name                    = "public_key_ec2"
   vpc_security_group_ids      = [aws_security_group.public.id]
   subnet_id                   = aws_subnet.public_subnet[0].id
-  tags                        = {
+  tags = {
     Name = "${var.env_code}-public"
   }
 }
 
-  resource "aws_security_group" "private" {
+resource "aws_security_group" "private" {
 
-    name   = "${var.env_code}-private-sec"
-    vpc_id = aws_vpc.main.id
+  name   = "${var.env_code}-private-sec"
+  vpc_id = aws_vpc.main.id
 
-    ingress {
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      cidr_blocks = ["${var.vpc_cidr}"]
-    }
-
-    egress {
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-    tags = {
-      Name = "${var.env_code}-private"
-    }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.vpc_cidr}"]
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "${var.env_code}-private"
+  }
+}
 
-  resource "aws_instance" "private" {
-    ami                         = "ami-0cff7528ff583bf9a"
-    instance_type               = "t3.micro"
-    associate_public_ip_address = true
-    key_name                    = "public_key_ec2"
-    vpc_security_group_ids      = [aws_security_group.private.id]
-    subnet_id                   = aws_subnet.private_subnet[0].id
-    tags = {
-      Name = "${var.env_code}-private"
-    }
+
+resource "aws_instance" "private" {
+  ami                         = "ami-0cff7528ff583bf9a"
+  instance_type               = "t3.micro"
+  associate_public_ip_address = true
+  key_name                    = "public_key_ec2"
+  vpc_security_group_ids      = [aws_security_group.private.id]
+  subnet_id                   = aws_subnet.private_subnet[0].id
+  tags = {
+    Name = "${var.env_code}-private"
+  }
 
 }
