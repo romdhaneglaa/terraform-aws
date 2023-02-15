195673f952a4da20d9bf1210fc44367ec2004217
romdhane <rglaa@bigapps.fr>
2023-02-15 15:57:09 +0100

    install 2048

--- user-data.sh
+++ user-data.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 yum update -y
-yum install -y httpd
-echo "yooloo" > /var/www/html/index.html
+yum install -y httpd git
+git clone https://github.com/gabrielecirulli/2048.git
+cp -R 2048/* /var/www/html
 systemctl enable httpd ; systemctl start httpd
