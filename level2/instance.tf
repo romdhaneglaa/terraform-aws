c7a5830132f3b5fdd57246ae68e94586e794b5c2
romdhane <rglaa@bigapps.fr>
2023-03-07 15:22:01 +0100

    clean code

--- level2/instance.tf
+++ level2/instance.tf
@@ -1,34 +1,4 @@
 
-resource "aws_security_group" "public" {
-
-  name   = "${var.env_code}-public-sec"
-  vpc_id = data.terraform_remote_state.level1.outputs.vpc_id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["${var.public_ip}"]
-  }
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["${var.public_ip}"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  tags = {
-    Name = "${var.env_code}-public"
-  }
-
-}
-
 resource "aws_instance" "private" {
   count                  = 2
   ami                    = data.aws_ami.amazonlinux.id
--- level2/loadbalancers.tf
+++ level2/loadbalancers.tf
@@ -17,12 +17,6 @@ resource "aws_security_group" "lb_sec_gpe" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
 
   egress {
