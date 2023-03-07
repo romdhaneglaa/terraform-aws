dcbf9ff8c3a1a5bc0a06e2d4027acc7bbf6f474d
romdhane <rglaa@bigapps.fr>
2023-03-07 15:27:55 +0100

    delete extra output file

--- level2/output.tf
+++ level2/output.tf
@@ -5,3 +5,7 @@
 output "private_ip" {
   value = aws_instance.private[*].private_ip
 }
+output "dns_name" {
+  value = aws_lb.lb_app.dns_name
+}
+
--- level2/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "dns_name" {
-  value = aws_lb.lb_app.dns_name
-}
-
