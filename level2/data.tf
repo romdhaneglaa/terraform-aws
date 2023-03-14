6028cc078bcf08f87f7f62b96ef16898ee6a75af
romdhane <rglaa@bigapps.fr>
2023-03-14 22:32:18 +0100

    get secret from data source

--- level2/data.tf
+++ level2/data.tf
@@ -6,3 +6,11 @@ data "terraform_remote_state" "level1" {
     region = "us-east-1"
   }
 }
+data "aws_secretsmanager_secret" "password" {
+  name = "rds/password"
+}
+data "aws_secretsmanager_secret_version" "version" {
+  secret_id = data.aws_secretsmanager_secret.password.id
+}
+
+
--- level2/rds.tf
+++ level2/rds.tf
@@ -4,7 +4,7 @@ module "rds" {
   vpc_id = data.terraform_remote_state.level1.outputs.vpc_id
   subnet_id = data.terraform_remote_state.level1.outputs.private_subnet
   db_name = "mydb"
-  db_psw = "adminadmin"
+  db_psw = jsondecode(data.aws_secretsmanager_secret_version.version.secret_string)["password"]
   db_user = "admin"
   secutiry_group = module.asg.instance_sec_gpe
 }
